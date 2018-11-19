98daba9a9b3635a5c151ac1d1ce14a43bd76fa7e
m.efimenko <mefimenko212@gmail.com>
2018-11-20 01:04:47 +0700

    Changed state strategy to GalleryView

--- app/src/main/java/com/test/photoeditor/presentation/gallery/GalleryView.kt
+++ app/src/main/java/com/test/photoeditor/presentation/gallery/GalleryView.kt
@@ -6,14 +6,12 @@ import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import com.test.photoeditor.domain.model.ImageItem
 
-@StateStrategyType(AddToEndSingleStrategy::class)
+@StateStrategyType(OneExecutionStateStrategy::class)
 interface GalleryView : MvpView {
     fun onShowImages(images: List<ImageItem>)
     fun onStartEditorActivity(imagePath: String)
     fun onCheckPermissionRationale(permission: String, requestCode: Int)
     fun onShowRationaleDialog(permission: String, requestCode: Int, text: String)
     fun onRequestForPermissions(permissions: Array<String>, requestCode: Int)
-
-    @StateStrategyType(OneExecutionStateStrategy::class)
     fun onShowMessage(message: String)
 }
